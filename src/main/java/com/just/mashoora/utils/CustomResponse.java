dfc6dbcb0fa91eaf171f675e19a7dfce925a5b92
Homam Manasra <homam.manasra26@gmail.com>
2024-03-11 02:52:43 +0300

    refactor(CustomResponse): Add new status field to the CustomReponse util class

--- src/main/java/com/just/mashoora/utils/CustomResponse.java
+++ src/main/java/com/just/mashoora/utils/CustomResponse.java
@@ -6,7 +6,13 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CustomResponse<T> {
+
+    private String status;
+
     private int statusCode;
+
     private String detailedStatusCode;
+
     private T data;
+
 }
